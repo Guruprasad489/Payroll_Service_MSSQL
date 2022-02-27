b6f419b99d1980726f96fc598d26dc5009e4b571
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-28 00:19:57 +0530

    [Guru] Welcome to Employee Payroll Program

---
+++ ADO.NetEmployeePayroll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetEmployeePayroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll service program");
+
+            Console.ReadLine();
+        }
+    }
+}
